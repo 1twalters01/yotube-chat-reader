9e4def3fdddaf4ed76c8701f7d97e39383afe36d
1twalters01 <1twalters01@gmail.com>
2023-02-03 22:57:55 +0000

    Excel works

--- reader.py
+++ reader.py
@@ -1,17 +1,44 @@
 import pytchat
-import time
-import json
+from openpyxl import Workbook, load_workbook
 
 link = "https://www.youtube.com/watch?v=MtQFR8xIfVI"
-video_id = link.replace('https://www.youtube.com/watch?v=','')
+file_name = "example.xlsx"
 
 
-chat = pytchat.create(video_id=video_id)
+try:
+    workbook = load_workbook(file_name)
+    worksheet = workbook.active
+except:
+    workbook = Workbook()
+    worksheet = link
+    worksheet['A1'].value = 'date and time'
+    worksheet['A2'].value = 'date and time'
+    worksheet['B2'].value = 'username'
+    worksheet['C2'].value = 'message'
+    workbook.save(file_name)
+
+
+for cell in worksheet["A"]:
+    if cell.value is None:
+        print(cell.row)
+        break
+else:
+    row = cell.row + 1
+print(row)
+
+    
+
+chat = pytchat.create(video_id=link.replace('https://www.youtube.com/watch?v=',''))
 while chat.is_alive():
     try:
         for c in chat.get().sync_items():
             if c.type == 'superChat':
-                print(f"{c.datetime} [{c.author.name}]- {c.message}")
+                print(f"{c.datetime} [{c.author.name}] - {c.message}")
+                worksheet['A'+str(row)].value = c.datetime
+                worksheet['B'+str(row)].value = c.author.name
+                worksheet['C'+str(row)].value = c.message
+                workbook.save(file_name)
+                row = row+1
     except AttributeError:
         print("Error occurred. Restarting...")
         chat = pytchat.create(video_id="uIx8l2xlYVY")
