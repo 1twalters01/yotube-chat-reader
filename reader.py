fe67b478da9e7be4944664269609cd6d7b0888e3
1twalters01 <1twalters01@gmail.com>
2023-02-03 21:15:36 +0000

    working

--- reader.py
+++ reader.py
@@ -1,3 +1,17 @@
 import pytchat
 import time
+import json
 
+link = "https://www.youtube.com/watch?v=MtQFR8xIfVI"
+video_id = link.replace('https://www.youtube.com/watch?v=','')
+
+
+chat = pytchat.create(video_id=video_id)
+while chat.is_alive():
+    try:
+        for c in chat.get().sync_items():
+            if c.type == 'superChat':
+                print(f"{c.datetime} [{c.author.name}]- {c.message}")
+    except AttributeError:
+        print("Error occurred. Restarting...")
+        chat = pytchat.create(video_id="uIx8l2xlYVY")
